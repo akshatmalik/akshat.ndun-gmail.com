a491a62188091a15f4aaefa8d81e36c2c442f082
Akshat Malik <akshat.ndun@gmail.com>
2020-04-02 08:13:28 +0530

    DP attempt

--- word_break.py
+++ word_break.py
@@ -5,22 +5,26 @@ class Solution:
     # @return an integer
     def find_make(self, A, a_index, B, b_index, dp):
 
-        if len(A) == a_index and len(B) == b_index:
-            return 1
+        if dp[a_index][b_index] == -1:
 
-        if a_index == len(A) and len(B) != b_index:
-            return 0
+            if len(A) == a_index and len(B) == b_index:
+                return 1
 
-        if a_index != len(A) and len(B) == b_index:
-            return 0
+            if a_index == len(A) and len(B) != b_index:
+                return 0
 
-        if A[a_index] == B[b_index]:
-            return self.find_make(A, a_index + 1, B, b_index + 1, dp) + self.find_make(A, a_index + 1, B, b_index, dp)
-        else:
-            return self.find_make(A, a_index + 1, B, b_index, dp)
+            if a_index != len(A) and len(B) == b_index:
+                return 0
+
+            if A[a_index] == B[b_index]:
+                dp[a_index][b_index] = self.find_make(A, a_index + 1, B, b_index + 1, dp) + self.find_make(A, a_index + 1, B, b_index, dp)
+            else:
+                dp[a_index][b_index] = self.find_make(A, a_index + 1, B, b_index, dp)
+
+        return dp[a_index][b_index]
 
     def numDistinct(self, A, B):
-        dp = [[-1 for _ in range(len(B))] for _ in range(len(A))]
+        dp = [[-1 for _ in range(len(B) + 1)] for _ in range(len(A) + 1) ]
         return self.find_make(A, 0, B, 0, dp)
 
 
@@ -33,6 +37,14 @@ if __name__ == "__main__":
     p = x.numDistinct("rabbbit", "rabbit")
     print("ans ", p)
     assert p == 3
+    x = Solution()
+    p = x.numDistinct("abc", "abc")
+    print("ans ", p)
+    assert p == 1
+    x = Solution()
+    p = x.numDistinct("cba", "abc")
+    print("ans ", p)
+    assert p == 0
     pr.disable()
     # after your program ends
     pr.print_stats(sort="calls")

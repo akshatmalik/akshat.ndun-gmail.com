3a1fdd43e77b1668b41b70a8cb74198fc88a6eaf
Akshat Malik <akshat.ndun@gmail.com>
2020-04-03 10:13:14 +0530

    Introduced memoization

--- interleaving_strings.py
+++ interleaving_strings.py
@@ -4,30 +4,36 @@ class Solution:
     # @param B : string
     # @param C : string
     # @return an integer
-    def find_iterleaving(self, a, a_index, b, b_index, c, c_index):
+    def find_iterleaving(self, a, a_index, b, b_index, c, c_index, dp):
 
-        # all are processed
-        if a_index == len(a) and b_index == len(b) and c_index == len(c):
-            return 1
+        key = "{}|{}|{}".format(a_index, b_index, c_index)
+        if dp.get(key, -1) == -1:
+            # all are processed
+            if a_index == len(a) and b_index == len(b) and c_index == len(c):
+                return 1
 
-        # b is left un processed
-        if a_index == len(a) and c_index == len(c):
-            return 0
+            # b is left un processed
+            if a_index == len(a) and c_index == len(c):
+                return 0
 
-        # a is left un processed
-        if b_index == len(b) and c_index == len(c):
-            return 0
+            # a is left un processed
+            if b_index == len(b) and c_index == len(c):
+                return 0
 
-        if a[a_index] == c[c_index]:
-            x = self.find_iterleaving(a_index + 1, a, b_index, b, c, c_index + 1)
+            x = y = 0
+            if a_index < len(a) and a[a_index] == c[c_index]:
+                x = self.find_iterleaving(a, a_index + 1, b, b_index, c, c_index + 1, dp)
 
-        if b[b_index] == c[c_index]:
-            y = self.find_iterleaving(a_index, a, b_index + 1, b, c, c_index + 1)
+            if b_index < len(b) and b[b_index] == c[c_index]:
+                y = self.find_iterleaving(a, a_index, b, b_index + 1, c, c_index + 1, dp)
 
-        return x or y
-        
+            dp[key] = x or y
+
+        return dp[key]
+
     def isInterleave(self, A, B, C):
-        return self.find_iterleaving(A, 0, B, 0, C, 0)
+        dp = {}
+        return self.find_iterleaving(A, 0, B, 0, C, 0, dp)
 
 
 if __name__ == "__main__":
@@ -36,7 +42,7 @@ if __name__ == "__main__":
     pr = cProfile.Profile()
     pr.enable()
     x = Solution()
-    p = x.isInterleave("aabcc", "dbbca", "aadbbcbcac")
+    p = x.isInterleave("aabcc", "bbca", "aabbcbcac")
     print("ans ", p)
     assert p == 1
     x = Solution()
@@ -53,4 +59,5 @@ if __name__ == "__main__":
     assert p == 1
     pr.disable()
     # after your program ends
+    pr.dump_stats("stats")
     pr.print_stats(sort="calls")
